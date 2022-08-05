8d6f41953ed3c172596731be9be0f392395a06ad
Klim Tsoutsman <klim@tsoutsman.com>
2022-08-05 22:02:29 +1000

    Play around with dependencies
    It works now!
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- library/std/src/sys/theseus/mod.rs
+++ library/std/src/sys/theseus/mod.rs
@@ -1,5 +1,25 @@
 #![deny(unsafe_op_in_unsafe_fn)]
 
+pub use libtheseus as _;
+
+/// This function exists to satisfy the linker.
+///
+/// `_start`, and thus this function are never called by Theseus.
+// TODO: Don't generate _start / don't call this function for Theseus.
+#[no_mangle]
+#[doc(hidden)]
+pub extern "C" fn __libc_start_main(
+    _main: usize,
+    _argc: i32,
+    _argv: usize,
+    _init_dummy: usize,
+    _fini_dummy: usize,
+    _ldso_dummy: usize,
+) -> i32 {
+    // Calling this function is an error.
+    1
+}
+
 pub mod alloc;
 pub mod args;
 #[path = "../unix/cmath.rs"]
